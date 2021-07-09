d23d044e2356ba148e995427879fd7c2d8986232
Diego Garcia Navas <diegogarcianavas.4b@gmail.com>
2021-07-09 18:37:56 +0200

    exercise 1.1 and 1.2

---
+++ Parte1/1.1/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({curso}) => {
+  return <div>
+    <h1>{curso}</h1>
+  </div>
+}
+
+const Content = ({part1,exercises1,part2,exercises2,part3,exercises3}) => {
+  return <div>
+    <p>{part1} {exercises1}</p>
+    <p>{part2} {exercises2}</p>
+    <p>{part3} {exercises3}</p>
+  </div>
+}
+
+const Total = ({total}) => {
+  return <div>
+    <p> Number of exercises {total}</p>
+  </div>
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const total = exercises1+exercises2+exercises3
+
+  return (
+    <div>
+      <Header curso={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total total={total} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
