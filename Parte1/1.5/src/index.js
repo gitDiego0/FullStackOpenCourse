2dffd8e658fd2d13e54420155940668aa695b140
Diego Garcia <diegogarcianavas.4b@gmail.com>
2021-08-18 12:17:08 +0200

    exercise 1.5

--- Parte1/1.5/src/index.js
+++ Parte1/1.5/src/index.js
@@ -22,7 +22,11 @@ const Content = ({parts}) => {
       })}
     </div>
   )
-  
+  // return <div>
+  //   <Part name={part1.name} exercises={part1.exercises} />
+  //   <Part name={part2.name} exercises={part2.exercises} />
+  //   <Part name={part3.name} exercises={part3.exercises} />
+  // </div>
 }
 
 const Total = ({parts}) => {
@@ -37,29 +41,25 @@ const Total = ({parts}) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
-  const {name,parts} = course
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <Header curso={name} />
+      <Header curso={course} />
       <Content parts={parts}  />
       <Total parts={parts} />
     </div>
