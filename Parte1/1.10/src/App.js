bd93337e9d905974e3b0d684a4434258fc7b79de
Diego Garcia <diegogarcianavas.4b@gmail.com>
2021-08-19 12:18:31 +0200

    exercise 1.10

---
+++ Parte1/1.10/src/App.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+
+const Button = (props) => {
+  return (
+    <button name={props.text} onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Statistics = ({ text, value }) => {
+  return (
+    <>
+      <p>{value}</p>
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClick = (e) => {
+    return e.target.name === 'Good'
+      ? setGood(good + 1)
+      : e.target.name === 'Neutral'
+      ? setNeutral(neutral + 1)
+      : setBad(bad + 1)
+  }
+
+  const all = good + neutral + bad
+  const average = 0 || (good - bad) / all
+  const positiveAverage = 0 || (good / all) * 100
+
+  return (
+    <div>
+      <div>
+        <h2>Feedback</h2>
+        <Button text="Good" onClick={handleClick} />
+        <Button text="Neutral" onClick={handleClick} />
+        <Button text="Bad" onClick={handleClick} />
+      </div>
+      <div>
+        <h2>Statics</h2>
+
+        {good === 0 && bad === 0 ? (
+          'No feedback given'
+        ) : (
+          <>
+            <Statistics text="good" value={good} />
+            <Statistics text="neutral" value={neutral} />
+            <Statistics text="bad" value={bad} />
+            <Statistics text="all" value={all} />
+            <Statistics text="average" value={average} />
+            <Statistics text="positiveAverage" value={positiveAverage} />
+          </>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
